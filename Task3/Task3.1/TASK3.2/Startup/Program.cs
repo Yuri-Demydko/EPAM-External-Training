7af4ca44cf42af128985edba01b4bfbdfa6b2ce0
demydko.yuri <yuridemydko@icloud.com>
2022-02-15 18:18:46 +0400

    3.2 done

---
+++ Task3/Task3.1/TASK3.2/Startup/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Data.Common;
+using DynamicArray;
+
+namespace Startup
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var basis = new[] {"1","2","3"};
+            var dyn = new DynamicArray<string>(basis);
+            //Console.WriteLine(dyn.Length);
+            dyn[10] = "olol";
+            
+            Console.WriteLine("Len: "+dyn.Length);
+            foreach (var d in dyn)
+            {
+                Console.Write(d + " ");
+            }
+
+            Console.WriteLine(dyn[102]);
+        }
+    }
+}
